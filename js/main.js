fb3ced65dada2b1b79efefb9e3c5d1da85e278d0
WilRocha97 <88037038+WilRocha97@users.noreply.github.com>
2023-05-16 15:35:46 -0300

    finalizando o js do robotron

--- js/main.js
+++ js/main.js
@@ -1,35 +1,10 @@
-function dizOla() {
-    console.log('Olá!')
-    console.log('Bem-vindo, eu sou o Robotron-2000!')
-}
-
-dizOla()
-
+//quanda o elemento da imagem do robotron
 const robotron = document.querySelector("#robotron")
-//robotron.addEventListener("click", function() {
-robotron.addEventListener("click", () => {
-    console.log('Olá!!')
-})
-
-//bloco responsável por adicionar função de soma e subtração de pontos de poder para cada aspecto do robotron
+//guarda os botões de mais e menos
 const controle = document.querySelectorAll("[data-controle]")
-controle.forEach((elemnto) => {
-    elemnto.addEventListener("click", (evento) => {
-        manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
-    })
-})
-
-function manipulaDados(operacao, controle) {
-    const peca = controle.querySelector("[data-contador]")
-    if(operacao === "-" && peca.value > 0) {
-        peca.value = parseInt(peca.value) - 1
-    }
-    if(operacao === "+" && peca.value < 50) {
-        peca.value = parseInt(peca.value) + 1
-    }
-}
-
-//bloco responsável por auterar as estatisticas do robotron conforme os pontos de poder distribuídos
+//guarda as estatisticas do robotron
+const estatisticas = document.querySelectorAll("[data-estatistica]")
+//objeto com os valores que seram alterados quando cada parte do robotron sofrer um upgrade ou downgrade
 const pecas = {
     "bracos": {
         "forca": 29,
@@ -63,3 +38,52 @@ const pecas = {
         "velocidade": -2
     }
 }
+
+//robotron.addEventListener("click", function() {
+robotron.addEventListener("click", () => {
+    console.log('Olá!!')
+})
+
+//bloco responsável por adicionar função de soma e subtração de pontos de poder para cada aspecto do robotron
+controle.forEach((elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode, evento.target.dataset.peca)
+
+        //verifica se as estatisticas estão menores que zero, se sim reverte a operação de soma
+        estatisticas.forEach((elemento) => {
+            elemento.textContent = parseInt(elemento.textContent)
+            if(elemento.textContent <= 0) {
+                alert(elemento.dataset.estatistica + ' Não pode ser menor que zero')
+                manipulaDados("-", evento.target.parentNode, evento.target.dataset.peca)
+            }
+        })
+    })
+})
+
+//função responsável por somar ou subtrair os pontos de poder do robotron
+function manipulaDados(operacao, controle, peca) {
+    const peca_upgrade = controle.querySelector("[data-contador]")
+    
+    if(operacao === "-" && peca_upgrade.value > 0) {
+        peca_upgrade.value = parseInt(peca_upgrade.value) - 1
+        atualizaEstatistica(operacao, controle, peca)
+    }
+    if(operacao === "+" && peca_upgrade.value < 10) {
+        peca_upgrade.value = parseInt(peca_upgrade.value) + 1
+        atualizaEstatistica(operacao, controle, peca)
+    }
+}
+
+//função responsável por auterar as estatisticas do robotron conforme os pontos de poder distribuídos
+function atualizaEstatistica(operacao, controle, peca) {
+    const peca_upgrade = controle.querySelector("[data-contador]")
+    estatisticas.forEach((elemento) => {
+        if(operacao === "-" && peca_upgrade.value >= 0) {
+            elemento.textContent = parseInt(elemento.textContent) - pecas[peca][elemento.dataset.estatistica]
+        }
+        if(operacao === "+" && peca_upgrade.value <= 10) {
+            elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica]
+        }
+    })
+}
+
